aa8b6d82538623f154bdf114a5cfcb6dd9c7f736
JohnEmerson1406 <john.carv.sousa@gmail.com>
2020-08-04 16:54:55 -0300

    feat: add components and pages

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -8,6 +8,8 @@ import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 
 import './styles.css';
 
+import { Link } from 'react-router-dom';
+
 function Landing() {
   return (
     <div id="page-landing">
@@ -24,15 +26,15 @@ function Landing() {
         />
 
         <div className="buttons-container">
-          <a href="" className="study" >
+          <Link to="/study" className="study" >
             <img src={studyIcon} alt="Estudar"/>
             Estudar
-          </a>
+          </Link>
 
-          <a href="" className="give-classes" >
+          <Link to="/give-classes" className="give-classes" >
             <img src={giveClassesIcon} alt="Dar aulas"/>
             Dar aulas
-          </a>
+          </Link>
         </div>
 
         <span className="total-connections">
