5149e752bf59898d70c72385b19d7e8dd0d81676
JohnEmerson1406 <john.carv.sousa@gmail.com>
2020-08-05 18:40:46 -0300

    feat: add ConnectionsController

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import ClassesController from './controllers/ClassesController';
+import ConnectionsController from './controllers/ConnectionsController';
 
 const routes = express.Router();
 const classesControllers = new ClassesController();
+const connectionsController = new ConnectionsController();
 
 routes.get('/', (req, res) => {
   return res.json({ message: 'Hello World' });
@@ -10,5 +12,8 @@ routes.get('/', (req, res) => {
 
 routes.get('/classes', classesControllers.index);
 routes.post('/classes', classesControllers.create);
+
+routes.get('/connections', connectionsController.index);
+routes.post('/connections', connectionsController.create);
 
 export default routes;
